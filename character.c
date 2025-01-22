eb06f86bdeb5925fe2197a573961cccdb2871088
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2025-01-22 16:30:28 +0530

    Add files via upload

---
+++ character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+char value;
+printf("\n Enter a character in capital letter:");
+value=getchar();
+value=value+32;
+printf("\n Small letter:%c",value);
+
+
+
+
+
+return 0;
+}
+
